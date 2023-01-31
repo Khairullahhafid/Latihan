e5927ea7a782d398fbea705575583d5d96a28d84
Khairullahhafid <khairuljamal12@gmail.com>
2023-01-31 10:31:03 +0800

    Menghapus search form yang salah penempatan

--- application/views/User/user_data.php
+++ application/views/User/user_data.php
@@ -14,16 +14,6 @@
         <div class="box-header">
             <h3 class="box-title"></h3>
             <div>
- <!-- search form -->
- <form action="#" method="get" class="sidebar-form">
-        <div class="input-group">
-          <input type="text" name="q" class="form-control" placeholder="Search...">
-          <span class="input-group-btn">
-                <button type="submit" name="search" id="search-btn" class="btn btn-flat"><i class="fa fa-search"></i>
-                </button>
-              </span>
-        </div>
-      </form>
       <!-- /.search form -->
             <div class="pull-right">
                 <a href="<?= site_url('user/add') ?>" class="btn btn-sm btn-primary">

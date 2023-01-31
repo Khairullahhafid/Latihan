11586be50d0a359f55ea36267ce0cee274a3460e
Khairullahhafid <khairuljamal12@gmail.com>
2023-01-31 11:01:37 +0800

    Menambahkan kolom pengisian pass dan button

--- application/views/User/user_tambah.php
+++ application/views/User/user_tambah.php
@@ -10,15 +10,38 @@
                   <label for="exampleInputEmail1">Email address</label>
                   <input type="email" class="form-control" id="exampleInputEmail1" placeholder="Enter email">
                 </div>
+                <div class="form-group <?=form_error ('password') ? 'has-error':null?>">
+                  <label for="Password">Password*</label>
+                  <input type="password" name="password" value="<?=set_value('password')?>" class="form-control" id="exampleInputPassword1" placeholder="Password">
+                </div>
+                <div class="form-group <?=form_error ('passkonf') ? 'has-error':null?>">
+                  <label for="Konfirmasi Password">Konfirmasi Password</label>
+                  <input type="password" name="passkonf" value="<?=set_value('passkonf')?>" class="form-control">
+                  <?=form_error ('passkonf')?>
+                </div>
                 <div class="form-group">
-                  <label for="exampleInputPassword1">Password</label>
-                  <input type="password" class="form-control" id="exampleInputPassword1" placeholder="Password">
+                  <label>Select</label>
+                  <select class="form-control">
+                    <option>option 1</option>
+                    <option>option 2</option>
+                    <option>option 3</option>
+                    <option>option 4</option>
+                    <option>option 5</option>
+                  </select>
                 </div>
                 
               </div>
               <!-- /.box-body -->
-
-              <div class="box-footer">
-                <button type="submit" class="btn btn-primary">Submit</button>
+              <div class="form-group">
+                <button type="submit" class="btn btn-succes"> 
+                    <i class="fa fa-paper-plane"></i> Simpan
+                </button>
+                <button type="reset" class="btn btn-warning"> 
+                    <i class="fa fa-paper-times"></i> Batal
+                </button>
+                <button type="submit" class="btn btn-succes"> 
+                    <i class="fa fa-paper-undo"></i> Kembali
+                </button>
               </div>
             </form>
+        

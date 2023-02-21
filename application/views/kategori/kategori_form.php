a5778b6738b421b577b3bacaeb7ee3e0138aef28
Khairullahhafid <khairuljamal12@gmail.com>
2023-02-21 10:26:03 +0800

    Menambahkan kategori_form.php

---
+++ application/views/kategori/kategori_form.php
@@ -0,0 +1,50 @@
+<section class="content-header">
+    <h1> Ketegori Barang
+        <small>Data</small>
+    </h1>
+    <ol class="breadcrumb">
+        <li><a href="#"><i class="fa fa-dashboard"></i> Home</a></li>
+        <li class="active">Kategori</li>
+    </ol>
+</section>
+
+<section class="content">
+    <div class = "box">
+        <div class = "box">
+            <div class = "box-header">
+                <h3 class = "box-title"></h3>
+            </div>
+
+            <div class="box-body table-reponsive">
+                <div class="row">
+                    <div class = "col-md-8 col-md-offset-2">
+                        <form class="form_horizontal" action="<?=site_url('kategori/proses')?>" method="post">
+
+                            <div class="form-group">
+                                <label class="col-sm-2 control-label">Nama Kategori</label>
+                                <div class="col-sm-10">
+                                    <input type="hidden" name="id" value="<?=$row->kategori_id?>">
+                                    <input type = "text" name="nama" value="<?=$row->nama?>"" class = "form-control" required>
+                                </div>
+                            </div>
+
+                            <div class="form-group">
+                                <div class="col-sm-offset-2 col-sm-10>
+                                <button type="submit" class="btn btn-success"> 
+                                        <i class="fa fa-paper-plane"></i> Simpan
+                                    </button>
+                                    <button type="reset" class="btn btn-warning"> 
+                                        <i class="fa fa-paper-times"></i> Batal
+                                    </button>
+                                    <button type="submit" class="btn btn-success"> 
+                                        <i class="fa fa-paper-undo"></i> Kembali
+                                    </button>
+                                </div>
+                            </div>
+                        </form>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>

291b813a6730c1ace8f57070bcdd72ed862fdd75
Khairullahhafid <khairuljamal12@gmail.com>
2023-02-21 11:28:23 +0800

    menambhkan kodingan di dalam kategori_m.php

---
+++ application/models/Kategori_m.php
@@ -0,0 +1,36 @@
+<?php
+defined('BASEPATH') or exit('No Direct Script Access Allowed');
+class User_m extends CI_Model
+{
+    public function get($id = null)
+    {
+        $this->db->select('*');
+        $this->db->from('tb_kategori');
+        if ($id != null) {
+            $this->db->where('kategori_id', $id);
+        }
+        $query = $this->db->get();
+        return $query;
+    }
+    public function add($post)
+    {
+        $params = [
+            'nama' => $post['nama'],
+        ];
+        $this->db->insert('tb_kategori',$params);
+    }
+    public function edit($post)
+    {
+        $params = [
+            'nama' => $post['nama'],
+            'updated' => date('Y-m-d H:i:s')
+        ];
+        $this->db->where('kategori_id', $post[$id]);
+        $this->db->update('tb_kategori', $params);
+    }
+    public function del($id)
+    {
+       $this->db->where('kategori_id', $id);
+       $this->db->delete('tb_kategori');
+    }
+}
